8f3adceceaddc3dfdfd51b108b4047ee4944a49a
Frédéric Testaniere <frederic.testaniere@etu.unice.fr>
2016-12-03 20:28:47 +0100

    Ajout d'une couleur dans l'interface du plugin

--- Plugins/src/main/java/plugins/Plugin_Graphique_Forme_Rectangle.java
+++ Plugins/src/main/java/plugins/Plugin_Graphique_Forme_Rectangle.java
@@ -1,11 +1,13 @@
 package plugins;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.io.Serializable;
 
 public class Plugin_Graphique_Forme_Rectangle implements Plugin_Graphique_Forme, Serializable {
 
-	public void dessinerForme(Graphics g, int x, int y, int width, int height) {
+	public void dessinerForme(Color c, Graphics g, int x, int y, int width, int height) {
+		g.setColor(c);
 		g.fillRect(x, y, 20, 20);
 	}
 
--- Plugins/src/main/java/plugins/Plugin_Graphique_Forme_Rond.java
+++ Plugins/src/main/java/plugins/Plugin_Graphique_Forme_Rond.java
@@ -1,12 +1,14 @@
 package plugins;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.io.Serializable;
 
 public class Plugin_Graphique_Forme_Rond implements Plugin_Graphique_Forme, Serializable {
 
-	public void dessinerForme(Graphics g, int x, int y, int width, int height) {
-		g.drawOval(x, y, width, height);
+	public void dessinerForme(Color c, Graphics g, int x, int y, int width, int height) {
+		g.setColor(c);
+		g.fillOval(x, y, width, height);
 	}
 
 }
