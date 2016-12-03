264c7258f3ee6984d3f8f595ff403e117beee154
laeticia-pierre <laeticia.pierre06200@gmail.com>
2016-12-03 23:11:27 +0100

    #19 TU deserialisation de la HashMap

--- Game/src/test/java/moteur/Test_SystemeSauvegarde.java
+++ Game/src/test/java/moteur/Test_SystemeSauvegarde.java
@@ -57,30 +57,36 @@ public class Test_SystemeSauvegarde {
 				);
 	}
 	
-	/*@Test
+	@Test
 	public void test_chargerSauvegarde() {
 		HashMap<String, Object> savePartie = systemeSauvegardeTest.chargerSauvegarde(new File("test_robotwarplay.ser"));
 		
 		ArrayList<HashMap<String, Object>> dataRobots = (ArrayList<HashMap<String, Object>>) savePartie.get("Robot");
 		//System.out.println("dataRobots : " + dataRobots);
-
+		
 		HashMap<String, Object> dataRobot1 = ((HashMap<String, Object>) dataRobots.get(0));
 		//System.out.println("dataRobot : " + dataRobot1);
 		
+		Point point = (Point) dataRobot1.get("position");
+		int cordX = point.x;
+		// Voir si les informations de chaque type sont recuperees correctement
 		assertTrue((Integer)dataRobot1.get("energie") == 10
-				&& (Integer)dataRobot1.get("x") >= 0 
-				&& (Integer)dataRobot1.get("y") >= 0
-				&& (Integer)dataRobot1.get("nombrePDV") == 10
+				&& dataRobot1.get("pluginCouleur").toString().contains("plugins.Plugin_Graphique_Couleur")
+				&& (Color) dataRobot1.get("couleur") != null
+				&& cordX >= 0
 				);
 		
 		HashMap<String, Object> dataRobot2 = ((HashMap<String, Object>) dataRobots.get(1));
-		//System.out.println("dataRobot : " + dataRobot2);
+		//System.out.println("dataRobot2 : " + dataRobot2);
 		
+		// ... S'il y en a plusieurs aussi
+		Point point2 = (Point) dataRobot2.get("position");
+		int cordX2 = point2.x;
 		assertTrue((Integer)dataRobot2.get("energie") == 10
-				&& (Integer)dataRobot2.get("x") >= 0 
-				&& (Integer)dataRobot2.get("y") >= 0
-				&& (Integer)dataRobot2.get("nombrePDV") == 10
+				&& dataRobot2.get("pluginCouleur").toString().contains("plugins.Plugin_Graphique_Couleur")
+				&& (Color) dataRobot2.get("couleur") != null
+				&& cordX2 >= 0
 				);
-	}*/
+	}
 	
 }
