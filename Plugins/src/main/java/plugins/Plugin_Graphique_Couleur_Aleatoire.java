13efbcf717277ea791c68cb3f03280d56580142d
Frédéric Testaniere <frederic.testaniere@etu.unice.fr>
2016-12-05 12:47:01 +0100

    correction de bug dans le plugin couleur aleatoire

--- Plugins/src/main/java/plugins/Plugin_Graphique_Couleur_Aleatoire.java
+++ Plugins/src/main/java/plugins/Plugin_Graphique_Couleur_Aleatoire.java
@@ -6,11 +6,15 @@ import java.io.Serializable;
 public class Plugin_Graphique_Couleur_Aleatoire implements Plugin_Graphique_Couleur, Serializable {
 
 	public Color getColor(Color c, int pv, int pvMax) {
+		
+		if(c == null){
 			int r = (int) Math.round(Math.random() * 255);
 			int g = (int) Math.round(Math.random() * 255);
 			int b = (int) Math.round(Math.random() * 255);
 			 
 			return new Color(r, g, b);
+		}
+		return c;
 	}
 
 }
